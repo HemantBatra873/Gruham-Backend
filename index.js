ca7174d8dc21d021ec6a31cf87b214143e494c92
HemantBatra873 <domspidy050204@gmail.com>
2024-07-24 10:43:09 +0530

    updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import userRouter from "./api/routes/user-routes.js"
 import authRouter from "./api/routes/auth-routes.js"
 import listingRouter from "./api/routes/listing-routes.js";
 import cookieParser from 'cookie-parser';
-import path from 'path';
 import cors from 'cors';
 dotenv.config();
 
@@ -15,12 +14,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 }); 
 
-const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.listen(5000 , ()=>{
     console.log("connected to 5000");
@@ -29,11 +27,6 @@ app.listen(5000 , ()=>{
 app.use("/api/user" , userRouter);
 app.use("/api/auth" , authRouter);
 app.use("/api/listing" , listingRouter);
-app.use(express.static(path.join(__dirname , 'client', 'dist')));
-
-app.get('/' , (req , res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 //middlewares
 app.use((err , req , res , next)=>{
